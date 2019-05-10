a6df6d0d5a687ac0eb922c497749290d8067923c
dibaskar <42174424+dibaskar@users.noreply.github.com>
2019-05-10 18:10:23 +0530

    Update new-rescue.sh
    updated the location details while generating snapshot and creating disk. Removed --attach-data-disks option while createing resue VM which was causing issues and added separate command to attach the disk

--- new-rescue.sh
+++ new-rescue.sh
@@ -154,14 +154,15 @@ else
     source_disk_name=`echo $disk_uri | awk -F"/" '{print $NF}'`
     snapshot_name="`echo $disk_uri | awk -F"/" '{print $NF}' | sed 's/_/-/g'`-`date +%d-%m-%Y-%T | sed 's/:/-/g'`"
     target_disk_name="`echo $disk_uri | awk -F"/" '{print $NF}'`-copy-`date +%d-%m-%Y-%T | sed 's/:/-/g'`"
-    az snapshot create -g $resource_group -n $snapshot_name --source $source_disk_name
+    az snapshot create -g $resource_group -n $snapshot_name --source $source_disk_name -l $location
 
     echo "##### Creating Disk from Snapshot #######"
 
     snapshotId=$(az snapshot show --name $snapshot_name --resource-group $resource_group --query [id] -o tsv)
-    az disk create --resource-group $resource_group --name $target_disk_name --sku Standard_LRS --source $snapshotId
+    az disk create --resource-group $resource_group --name $target_disk_name -l $location --sku Standard_LRS --source $snapshotId
 
-    az vm create --name $rn -g $g --location $location --attach-data-disks $target_disk_name --admin-username $user --admin-password $password --image $urn --storage-sku Standard_LRS 
+    az vm create --name $rn -g $g --location $location --admin-username $user --admin-password $password --image $urn --storage-sku Standard_LRS
+    az vm disk attach -g $g --vm-name $rn --name $target_disk_name
 fi
  
 
