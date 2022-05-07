aa6b07e591026d79726da4a006cbd0bd3ea3c9b6
d-sergienko <d-sergienko@users.noreply.github.com>
2022-05-07 14:46:54 +0300

    Fixed errors (#13)
    * 1. fix warning about outdated parameter name; 2. fix disk name length (fix error "The value of parameter disk.name is invalid."
    
    * fix error querying the image of "null:null:null:null"
    
    Co-authored-by: Dmitry Sergienko <serhiienko.d@novaposhta.ua>

--- new-rescue.sh
+++ new-rescue.sh
@@ -129,6 +129,14 @@ sku=$(echo $vm_details | jq ".storageProfile.imageReference.sku")
 version=$(echo $vm_details | jq ".storageProfile.imageReference.version")
 
 urn=$(echo "${publisher//\"}:${offer//\"}:${sku//\"}:${version//\"}")
+# fix null values
+if [ `echo "$urn" | grep -q null; echo $?` -eq 0 ] ; then
+    publisher="canonical"
+    offer="0001-com-ubuntu-server-focal"
+    sku="20_04-lts-gen2"
+    version="latest"
+    urn=$(echo "${publisher//\"}:${offer//\"}:${sku//\"}:${version//\"}")
+fi
 echo $urn
 #echo $managed
 disk_uri="null"
@@ -153,7 +161,7 @@ else
     echo "##### Generatnig Snapshot #######"
     source_disk_name=`echo $disk_uri | awk -F"/" '{print $NF}'`
     snapshot_name="`echo $disk_uri | awk -F"/" '{print $NF}' | sed 's/_/-/g'`-`date +%d-%m-%Y-%T | sed 's/:/-/g'`"
-    target_disk_name="`echo $disk_uri | awk -F"/" '{print $NF}'`-copy-`date +%d-%m-%Y-%T | sed 's/:/-/g'`"
+    target_disk_name="`echo $disk_uri | awk -F"/" '{print $NF}'`.`date +%s`"
     az snapshot create -g $resource_group -n $snapshot_name --source $source_disk_name -l $location
 
     echo "##### Creating Disk from Snapshot #######"
@@ -162,7 +170,7 @@ else
     az disk create --resource-group $resource_group --name $target_disk_name -l $location --sku Standard_LRS --source $snapshotId
 
     az vm create --name $rn -g $g --location $location --admin-username $user --admin-password $password --image $urn --storage-sku Standard_LRS
-    az vm disk attach -g $g --vm-name $rn --disk  $target_disk_name
+    az vm disk attach -g $g --vm-name $rn --name  $target_disk_name
 fi
  
 
